adeb32a1fc18e7b5b0bfd2830d65bf0381bda525
clara <clara@clara.com>
2018-02-07 12:29:42 -0600

    mocking api example

---
+++ mock_api/movie.py
@@ -0,0 +1,71 @@
+'''
+Install requests with
+pip install requests
+'''
+
+import requests
+import os
+
+
+'''
+Before running this code, create an OMDB key
+set the OMDB_KEY environment variable on your computer
+You can do it temporarily with the appropriate command,
+replacing abcd1234 with your key
+
+(PC)
+
+set OMDB_KEY=abcd1234
+
+(Mac)
+
+export OMDB_KEY=abcd1234
+
+Or, you can set permanently. Google will tell you how.
+'''
+
+class OMDB_Exception(Exception):
+    pass
+
+def omdb_call(movie_name):
+    key = os.environ['OMDB_KEY']  # Read key from environment variable
+    base_url = 'http://www.omdbapi.com/'
+    params = { 'apikey' : key, 't' : movie_name }
+    return requests.get(base_url, params ).json()
+
+def movie_rating(movie_name):
+
+    try:
+        data = omdb_call(movie_name)
+    except Exception as e:
+        raise OMDB_Exception('Error connecting to OMDB')
+
+    # Valid responses reporting errors from the query include response has a field 'Response' which is true or false, and an error message
+    # {'Response': 'False', 'Error': 'Movie not found!'}
+
+    # An example valid response - with no "Response" field
+    # {'Plot': 'When the newly-crowned Queen Elsa...', 'imdbVotes': '475,585', 'Released': '27 Nov 2013', 'imdbRating': '7.5', 'Rated': 'PG', 'Awards': 'Won 2 Oscars. Another 77 wins & 57 nominations.', 'Production': 'Walt Disney Pictures', 'BoxOffice': '$400,736,600', 'Website': 'http://www.disney.com/frozen', 'Year': '2013', 'Metascore': '74', 'Director': 'Chris Buck, Jennifer Lee', 'Title': 'Frozen', 'Actors': 'Kristen Bell, Idina Menzel, Jonathan Groff, Josh Gad', 'Country': 'USA', 'Response': 'True', 'imdbID': 'tt2294629', 'Runtime': '102 min', 'Genre': 'Animation, Adventure, Comedy', 'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1MjQwMTE5OF5BMl5BanBnXkFtZTgwNjk3MTcyMDE@._V1_SX300.jpg', 'Ratings': [{'Value': '7.5/10', 'Source': 'Internet Movie Database'}, {'Value': '89%', 'Source': 'Rotten Tomatoes'}, {'Value': '74/100', 'Source': 'Metacritic'}], 'Language': 'English, Norwegian', 'Writer': 'Jennifer Lee (screenplay by), Hans Christian Andersen (story inspired by "The Snow Queen" by), Chris Buck (story by), Jennifer Lee (story by), Shane Morris (story by)', 'Type': 'movie', 'DVD': '18 Mar 2014'}
+
+    try:
+        if data:
+            if 'Response' in data:
+                error_message = data['Error']
+                raise OMDB_Exception(error_message)
+            else:
+                return data['Ratings'][0]['Value']
+
+    except (KeyError, TypeError, IndexError) as e:
+        raise OMDB_Exception('Error processing response from OMDB')
+
+
+
+def main():
+    movie_name = input('Please enter movie name: ')
+    try:
+        rating = movie_rating(movie_name)
+    except OMDB_Exception as e:
+        print('Error getting rating, ' + str(e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ mock_api/movie_test.py
@@ -0,0 +1,80 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+import movie
+from movie import movie_rating, OMDB_Exception, omdb_call
+
+class Test_OMDB_API(TestCase):
+
+
+    # Patching with a decorator. Creates a mock for the designated function for the duration of this test, unpatches at the end
+    # Note the second argument to the test function is required, makes the Mock object available to the test
+    @patch('movie.omdb_call', return_value = {'Ratings': [{'Value': '7.5/10', 'Source': 'Internet Movie Database'}]} )
+    def test_get_movie_rating_patch(self, api_patch):
+        response = movie_rating('Example Movie')
+        self.assertEqual('7.5/10', response)
+
+
+    # Same test, but uses a mock
+    def test_get_movie_rating_mock(self):
+
+        # Create a mock and replace movie.omdb_call with the Mock function
+        mock_api = Mock(return_value = {'Ratings': [{'Value': '7.5/10', 'Source': 'Internet Movie Database'}]} )
+        movie.omdb_call = mock_api
+        response = movie_rating('Example Movie')
+        self.assertEqual('7.5/10', response)
+
+        # If needed, can check how the mock was called. For example, was it called at all?
+        mock_api.assert_called()   # New in python 3.6 so check version and upgrade if needed.
+        # See documentation for checking for a certain number of calls, verifying mock was called with expected arguments.
+
+    # Same test, but uses patch as a context manager
+    def test_get_movie_rating_patch_decorator(self):
+        with patch('movie.omdb_call', return_value = {'Ratings': [{'Value': '7.5/10', 'Source': 'Internet Movie Database'}]} ) as mock_api:
+            response = movie_rating('Example Movie')
+            self.assertEqual('7.5/10', response)
+
+        # At this point in time, the patch is no longer applied. Use this approach if you only need to patch for part of a test.
+
+
+    # Some more tests - was exception thrown if request errors?
+    # Raise general exception from omdb_call
+    @patch('movie.omdb_call', side_effect=Exception)
+    def test_get_movie_rating_error_connecting(self, api_patch):
+        with self.assertRaises(movie.OMDB_Exception) as ex_context:
+            response = movie_rating('Example Movie')
+        self.assertEqual('Error connecting to OMDB', str(ex_context.exception))
+
+
+    # Was exception thrown if response contains an error?
+    @patch('movie.omdb_call', side_effect=[{'Response':'false', 'Error':'Something went wrong'}])
+    def test_get_movie_rating_error_returned_in_response(self, api_patch):
+
+        with self.assertRaises(movie.OMDB_Exception) as ex_context:
+            response = movie_rating('Example Movie')
+
+        self.assertEqual('Something went wrong', str(ex_context.exception))
+
+
+    # Expect an exception thrown if json is not in expected format for Ratings
+    @patch('movie.omdb_call', return_value = {'Ratings': '7.5'} )
+    def test_get_movie_rating_ratings_incorrect_format(self, api_patch):
+        with self.assertRaises(OMDB_Exception) as ex_context:
+            movie.movie_rating('example')
+        self.assertEqual("Error processing response from OMDB", str(ex_context.exception))
+
+
+    # Expect an exception thrown if json is not in expected format for Ratings
+    @patch('movie.omdb_call', return_value = {'Ratings': [] } )
+    def test_get_movie_rating_no_ratings_returned(self, api_patch):
+        with self.assertRaises(OMDB_Exception) as ex_context:
+            movie.movie_rating('example')
+        self.assertEqual("Error processing response from OMDB", str(ex_context.exception))
+
+
+    # Expect an exception thrown if json is not in expected format for Ratings or response
+    @patch('movie.omdb_call', return_value = {'Donut': 'Sprinkles'} )
+    def test_get_movie_rating_no_response_or_rating(self, api_patch):
+        with self.assertRaises(OMDB_Exception) as ex_context:
+            movie.movie_rating('example')
+        self.assertEqual("Error processing response from OMDB", str(ex_context.exception))
