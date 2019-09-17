1d6f148b858729c57bb01692b1fb29bcccdb1520
clara <clara@minneapolis.edu>
2019-09-17 18:48:31 -0500

    update to context manager, fix spacing, extra comments

--- miles_db/test_miles.py
+++ miles_db/test_miles.py
@@ -1,15 +1,17 @@
-
-import mileage
-from mileage import MileageError
 import sqlite3
+import unittest
 from unittest import TestCase
+
+import miles
+from miles import MileageError
+
 
 class TestMileageDB(TestCase):
 
     test_db_url = 'test_miles.db'
 
     """
-    Before running this test, create test_miles.db
+    Before running these test, create test_miles.db
     Create expected miles table
     create table miles (vehicle text, total_miles float);
     """
@@ -17,30 +19,29 @@ class TestMileageDB(TestCase):
     # The name of this method is important - the test runner will look for it
     def setUp(self):
         # Overwrite the mileage
-        mileage.db_url = self.test_db_url
+        miles.db_url = self.test_db_url
         # drop everything from the DB to always start with an empty database
-        conn = sqlite3.connect(self.test_db_url)
-        conn.execute('DELETE FROM miles')
-        conn.commit()
+        with sqlite3.connect(self.test_db_url) as conn:
+            conn.execute('DELETE FROM miles')
         conn.close()
 
 
     def test_add_new_vehicle(self):
-        mileage.add_miles('Blue Car', 100)
+        miles.add_miles('Blue Car', 100)
         expected = { 'Blue Car': 100 }
         self.compare_db_to_expected(expected)
 
-        mileage.add_miles('Green Car', 50)
+        miles.add_miles('Green Car', 50)
         expected['Green Car'] = 50
         self.compare_db_to_expected(expected)
 
 
     def test_increase_miles_for_vehicle(self):
-        mileage.add_miles('Red Car', 100)
+        miles.add_miles('Red Car', 100)
         expected = { 'Red Car': 100 }
         self.compare_db_to_expected(expected)
 
-        mileage.add_miles('Red Car', 50)
+        miles.add_miles('Red Car', 50)
         expected['Red Car'] = 100 + 50
         self.compare_db_to_expected(expected)
 
@@ -52,11 +53,11 @@ class TestMileageDB(TestCase):
 
     def test_add_new_vehicle_invalid_new_miles(self):
         with self.assertRaises(MileageError):
-            mileage.add_miles('Car', -100)
+            miles.add_miles('Car', -100)
         with self.assertRaises(MileageError):
-            mileage.add_miles('Car', 'abc')
+            miles.add_miles('Car', 'abc')
         with self.assertRaises(MileageError):
-            mileage.add_miles('Car', '12.def')
+            miles.add_miles('Car', '12.def')
 
 
     # This is not a test method, instead, it's used by the test methods
@@ -75,3 +76,7 @@ class TestMileageDB(TestCase):
             self.assertEqual(expected[row[0]], row[1])
 
         conn.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
