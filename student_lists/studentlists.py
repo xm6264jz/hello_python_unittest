108794bb0945b12f73525cbc8a66d1c431d17ef5
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-09-20 13:11:45 -0500

    Raise StudentError if the maximum students is zero or less

--- student_lists/studentlists.py
+++ student_lists/studentlists.py
@@ -14,11 +14,14 @@ class ClassList:
 
     def __init__(self, max_students):
 
+        if max_students <=0:
+            raise StudentError("Number of students must be greater than 0")
+
+
         self.class_list = []
         self.max_students = max_students
 
-        # TODO raise an exception - StudentError - if max_students is zero or negative. 
-        # Write test to confirm.
+       
 
 
     def add_student(self, student):
